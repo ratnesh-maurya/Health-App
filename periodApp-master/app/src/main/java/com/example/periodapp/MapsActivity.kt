37be7f54ec2457a6d68e11900c2cb98eaf86c5c4
Ratnesh Maurya <85143283+ratnesh-maurya@users.noreply.github.com>
2022-10-19 23:37:17 +0530

    update

--- periodApp-master/app/src/main/java/com/example/periodapp/MapsActivity.kt
+++ periodApp-master/app/src/main/java/com/example/periodapp/MapsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.periodapp
 
+import android.graphics.Color
+import android.location.Geocoder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -10,6 +12,7 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.periodapp.databinding.ActivityMapsBinding
+import com.google.android.gms.maps.model.CircleOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -22,27 +25,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val kanpur = LatLng(26.4189, 80.3085)
+        mMap.addMarker(MarkerOptions().position(kanpur).title("Marker in kanpur"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(kanpur))
+
+        mMap.addCircle( CircleOptions()
+            .center(kanpur).radius(1000.0).fillColor(Color.BLUE).strokeColor(Color.MAGENTA))
+
+       
     }
 }

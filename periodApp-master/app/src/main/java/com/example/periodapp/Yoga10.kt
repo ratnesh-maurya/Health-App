9a2498cacbe250cb2b216cc133cc5b295cdacd5c
harshit <harshitgsrivastava07gmail.com>
2022-10-30 08:48:26 +0530

    Age grouping added

---
+++ periodApp-master/app/src/main/java/com/example/periodapp/Yoga10.kt
@@ -0,0 +1,12 @@
+package com.example.periodapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Yoga10 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_yoga10)
+        title="yoga for 10-18 yr "
+    }
+}

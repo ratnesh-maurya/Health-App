e9b0b7dca8992458c89f9069f48e831152c87a3c
harshit <harshitgsrivastava07gmail.com>
2022-11-05 19:40:56 +0530

    yoga input part done

--- periodApp-master/app/src/main/java/com/example/periodapp/YogaAgegroup.kt
+++ periodApp-master/app/src/main/java/com/example/periodapp/YogaAgegroup.kt
@@ -2,33 +2,45 @@ package com.example.periodapp
 
 import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class YogaAgegroup : AppCompatActivity() {
     lateinit var button1: Button
     lateinit var button2: Button
     lateinit var  button3: Button
+    lateinit var edittext:EditText
+    lateinit var  button4:Button
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_yoga_agegroup)
 
-        button1=findViewById(R.id.button10)
-        button1.setOnClickListener {
+        edittext=findViewById(R.id.Age)
+        button4=findViewById(R.id.Clickme)
+        button4.setOnClickListener {
+            val ans: Int = edittext.getText().toString().toInt()
+            if(ans>8 && ans<=18){
             val intent = Intent(this,Yoga10::class.java)
             startActivity(intent)
+              }
+            if(ans>18 && ans<=45){
+                val intent = Intent(this,Yoga::class.java)
+                startActivity(intent)
+            }
+            if(ans>45){
+                val intent = Intent(this,Yoga45::class.java)
+                startActivity(intent)
+            }
         }
-        button2=findViewById(R.id.button18)
-        button2.setOnClickListener {
-            val intent = Intent(this,Yoga::class.java)
-            startActivity(intent)
-        }
-        button3=findViewById(R.id.button45)
-        button3.setOnClickListener {
-            val intent = Intent(this,Yoga45::class.java)
-            startActivity(intent)
-        }
+
+
+
+
+
+
+
     }
 }
